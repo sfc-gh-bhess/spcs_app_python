bc59b633aabb72b1164a627b2957f649be55f04f
brian.hess <brian.hess@snowflake.com>
2023-12-07 16:37:49 -0500

    typos

--- backend/src/spcs_helpers/connection.py
+++ backend/src/spcs_helpers/connection.py
@@ -11,7 +11,7 @@ def connection() -> snowflake.connector.SnowflakeConnection:
             'account': os.getenv('SNOWFLAKE_ACCOUNT'),
             'authenticator': "oauth",
             'token': open('/snowflake/session/token', 'r').read(),
-            'warehouse': args['warehouse'],
+            'warehouse': os.getenv('SNOWFLAKE_WAREHOUSE'),
             'database': os.getenv('SNOWFLAKE_DATABASE'),
             'schema': os.getenv('SNOWFLAKE_SCHEMA'),
             'client_session_keep_alive': True
